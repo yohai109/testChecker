a8e2e0b459f6961fc6c87d8a1d74ff063c9af87b
Yohai Knaani <yohai109@gmail.com>
2018-10-09 19:49:51 +0300

    add first try of script. not tested yet.

--- testChecker.py
+++ testChecker.py
@@ -1,1 +1,27 @@
+import os
+import shutil
 
+dirOfFiles = raw_input("Please enter the directory with the files:")
+numOfLinesToRead = input("please enter the number of liens to read from the file")
+filesToCheck = os.listdir(dirOfFiles)
+
+filesToCheckNew = []
+for i in filesToCheck:
+	if i.endswith(".py"):
+		filesToCheckNew.append(i)
+
+for i in filesToCheckNew:
+	csvLine = ""
+	with open(i, 'r') as currFile:
+		fileData = currFile.readlines(int(numOfLinesToRead))
+
+	fileLines = fileData
+
+	for currLine in fileLines:
+		if currLine[0] == "#":
+			csvLine += currLine.split(" ")[-1] + ","
+
+	csvLine = csvLine[0:-2]
+
+	with open(".\\solutions.csv", 'w') as csvFile:
+		csvFile.write(csvLine)
